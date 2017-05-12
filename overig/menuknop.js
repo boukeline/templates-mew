5571d30fa3b327f6be132edfedce5ce3030ebb2c
boukeline <boukelinearnold@MacBook-Air-van-Boukeline.local>
2017-05-12 20:43:36 +0100

    t3

---
+++ overig/menuknop.js
@@ -0,0 +1,7 @@
+function toggle(id) {
+		   var e = document.getElementById('hoofdmenu');
+		   if(e.style.display === 'block')
+			  e.style.display = 'none';
+		   else
+			  e.style.display = 'block';
+		}
